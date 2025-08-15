27b1eb129299c2cf08fc02467c8c1b2a7a722a84
Divyansh Dewangan <dewangandivyansh799@gmail.com>
2025-08-16 00:15:10 +0530

    Favorites program and tests done

---
+++ basics/Favorites/anchor/favorites/tests/system-error.ts
@@ -0,0 +1,11 @@
+export const systemProgramErrors = [
+    'an account with the same address already exists',
+    'account does not have enough sol to perform the operation',
+    'canoot assing the account to this program id',
+    'cannot allocate account data of this length',
+    'length of requested seed is too long',
+    'provided address does not match addressed derived from seed',
+    'advancing stored nonce requires a populated RecentBlockhash sysvar',
+    'stored nonce is still in recent_blockhash',
+    'specified nonce does not match stored nonce'
+]
